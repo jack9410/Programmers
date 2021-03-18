96779d1cd0b210fb33cebd2426db8e48547bff91
jack9410 <46255505+jack9410@users.noreply.github.com>
2021-03-18 15:32:25 +0900

    Update 메뉴_리뉴얼.py
    다른풀이 주석추가

--- 2021_Kakao/메뉴_리뉴얼.py
+++ 2021_Kakao/메뉴_리뉴얼.py
@@ -1,3 +1,6 @@
+# 브루트 포스
+# 그냥 모든 조합 다 세보고 가장 많은거 찾는다
+
 from itertools import combinations
 
 def solution(orders, course):
@@ -33,6 +36,28 @@ def solution(orders, course):
     return answer
 
 
-orders = ["XYZ", "XWY", "WXA"]
-course = [2,3,4]	
-solution(orders, course)
+# orders = ["XYZ", "XWY", "WXA"]
+# course = [2,3,4]	
+# solution(orders, course)
+
+
+# 다른 사람의 풀이
+# Counter 이용한다.
+
+# from itertools import combinations
+# from collections import Counter
+# def solution(orders, course):
+#     answer = []
+#     for k in course:
+#         candidates = []
+#         for menu_li in orders:
+#             for li in combinations(menu_li, k):
+#                 res = ''.join(sorted(li))
+#                 candidates.append(res)
+#         sorted_candidates = Counter(candidates).most_common()
+#         answer += [menu for menu, cnt in sorted_candidates if cnt > 1 and cnt == sorted_candidates[0][1]]
+#     return sorted(answer)
+
+# orders = ["XYZ", "XWY", "WXA"]
+# course = [2,3,4]	
+# print(solution(orders, course))
